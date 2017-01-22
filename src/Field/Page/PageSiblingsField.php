159276307e9954f2b04fabf7d8e875529132ea03
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-22 21:02:12 +0600

    Add optional selector argument support.

--- src/Field/Page/PageSiblingsField.php
+++ src/Field/Page/PageSiblingsField.php
@@ -5,6 +5,9 @@ namespace ProcessWire\GraphQL\Field\Page;
 use Youshido\GraphQL\Field\AbstractField;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Field\InputField;
+use Youshido\GraphQL\Config\Field\FieldConfig;
+use ProcessWire\GraphQL\Type\Scalar\SelectorType;
 use ProcessWire\GraphQL\Type\Object\PageArrayType as PageArrayObjectType;
 
 class PageSiblingsField extends AbstractField {
@@ -21,11 +24,21 @@ class PageSiblingsField extends AbstractField {
 
   public function getDescription()
   {
-    return "All the sibling pages of this page. Returns a PageArray.";
+    return "All the sibling pages of this page. Optionally filter them with selector. Returns a PageArray.";
+  }
+
+  public function build(FieldConfig $config)
+  {
+    $config->addArgument(new InputField([
+      'name' => SelectorType::ARGUMENT_NAME,
+      'type' => new SelectorType(),
+    ]));
   }
 
   public function resolve($value, array $args, ResolveInfo $info)
   {
+    $s = SelectorType::ARGUMENT_NAME;
+    if (isset($args[$s])) return $value->siblings($args[$s]);
     return $value->siblings;
   }
 
