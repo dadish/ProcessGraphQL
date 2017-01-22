c90448bfb98d11ff86cfaa47b55f645d560ea143
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-22 19:42:58 +0600

    Add optional selector argument support.

--- src/Field/Page/PageChildField.php
+++ src/Field/Page/PageChildField.php
@@ -4,6 +4,9 @@ namespace ProcessWire\GraphQL\Field\Page;
 
 use Youshido\GraphQL\Field\AbstractField;
 use Youshido\GraphQL\Execution\ResolveInfo;
+use Youshido\GraphQL\Field\InputField;
+use Youshido\GraphQL\Config\Field\FieldConfig;
+use ProcessWire\GraphQL\Type\Scalar\SelectorType;
 use ProcessWire\GraphQL\Type\Object\PageType as PageObjectType;
 use ProcessWire\NullPage;
 
@@ -21,12 +24,25 @@ class PageChildField extends AbstractField {
 
   public function getDescription()
   {
-    return 'The first child of this page. Returns a Page or null.';
+    $description = 'The first child of this page. ';
+    $description .= 'If the `s`(selector) argument is provided then the first matching child (subpage) that matches the given selector. ';
+    $description .= 'Returns a Page or null.';
+    return $description;
+  }
+
+  public function build(FieldConfig $config)
+  {
+    $config->addArgument(new InputField([
+      'name' => SelectorType::ARGUMENT_NAME,
+      'type' => new SelectorType(),
+    ]));
   }
 
   public function resolve($value, array $args, ResolveInfo $info)
   {
-    $child = $value->child;
+    $s = SelectorType::ARGUMENT_NAME;
+    if (isset($args[$s])) $child = $value->child($args[$s]);
+    else $child = $value->child;
     if ($child instanceof NullPage) return null;
     return $child;
   }
