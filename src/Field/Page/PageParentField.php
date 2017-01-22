4b12d114310e36d2e022ebe6857d61ff876f3e15
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-22 21:06:34 +0600

    Add optional selector argument support.

--- src/Field/Page/PageParentField.php
+++ src/Field/Page/PageParentField.php
@@ -4,6 +4,9 @@ namespace ProcessWire\GraphQL\Field\Page;
 
 use Youshido\GraphQL\Field\AbstractField;
 use Youshido\GraphQL\Execution\ResolveInfo;
+use Youshido\GraphQL\Field\InputField;
+use Youshido\GraphQL\Config\Field\FieldConfig;
+use ProcessWire\GraphQL\Type\Scalar\SelectorType;
 use ProcessWire\GraphQL\Type\Object\PageType as PageObjectType;
 use ProcessWire\NullPage;
 
@@ -21,12 +24,22 @@ class PageParentField extends AbstractField {
 
   public function getDescription()
   {
-    return 'The parent Page object or a `null` if there is no parent.';
+    return 'The parent Page object, or the closest parent matching the given selector. Returns `null` if there is no parent or no match.';
+  }
+
+  public function build(FieldConfig $config)
+  {
+    $config->addArgument(new InputField([
+      'name' => SelectorType::ARGUMENT_NAME,
+      'type' => new SelectorType(),
+    ]));
   }
 
   public function resolve($value, array $args, ResolveInfo $info)
   {
-    $parent = $value->parent;
+    $s = SelectorType::ARGUMENT_NAME;
+    if (isset($args[$s])) $parent = $value->parent($args[$s]);
+    else $parent = $value->parent;
     if ($parent instanceof NullPage) return null;
     return $parent;
   }
