f27ebcd0111889a501d96a192eb7d68778aa1f2b
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-22 19:02:41 +0600

    Enable argument support for `numChildren` field.

--- src/Field/Page/PageNumChildrenField.php
+++ src/Field/Page/PageNumChildrenField.php
@@ -2,12 +2,17 @@
 
 namespace ProcessWire\GraphQL\Field\Page;
 
+use Youshido\GraphQL\Config\Field\FieldConfig;
 use Youshido\GraphQL\Field\AbstractField;
+use Youshido\GraphQL\Field\InputField;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Type\Scalar\IntType;
+use Youshido\GraphQL\Type\Scalar\BooleanType;
 use Youshido\GraphQL\Type\NonNullType;
 
 class PageNumChildrenField extends AbstractField {
+
+  const firstArgumentName = 'visible';
 
   public function getType()
   {
@@ -24,8 +29,19 @@ class PageNumChildrenField extends AbstractField {
     return "The number of children (subpages) this page has.";
   }
 
+  public function build(FieldConfig $config)
+  {
+    $config->addArgument(new InputField([
+      'name' => self::firstArgumentName,
+      'type' => new BooleanType(),
+      'default' => false,
+    ]));
+  }
+
   public function resolve($value, array $args, ResolveInfo $info)
   {
+    $visible = $args[self::firstArgumentName];
+    if ($visible) return $value->numChildren($visible);
     return $value->numChildren;
   }
 
