e22c58cd6996de205399ff9164eab496185ebcb6
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-23 19:14:49 +0600

    Fix the pages.count API.

--- src/Field/Pages/PagesCountField.php
+++ src/Field/Pages/PagesCountField.php
@@ -3,13 +3,13 @@
 namespace ProcessWire\GraphQL\Field\Pages;
 
 use Youshido\GraphQL\Execution\ResolveInfo;
-use ProcessWire\GraphQL\Field\WireArray\WireArrayFindField;
+use ProcessWire\GraphQL\Field\WireArray\WireArrayCountField;
 
-class PagesCountField extends WireArrayFindfield {
+class PagesCountField extends WireArrayCountfield {
 
   public function getDescription()
   {
-    return 'Count and return how many pages will match the given selector or all pages if no selector given.';
+    return 'Count and return how many pages will match the given selector or all pages of the site if no selector given.';
   }
 
 }
