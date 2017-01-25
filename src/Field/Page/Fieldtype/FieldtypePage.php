09c6a3a98a1b85eaafb5eeeb4bf9d0547beeddd8
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-25 13:46:49 +0600

    Add basic support for FieldtypePage.

--- src/Field/Page/Fieldtype/FieldtypePage.php
+++ src/Field/Page/Fieldtype/FieldtypePage.php
@@ -2,14 +2,42 @@
 
 namespace ProcessWire\GraphQL\Field\Page\Fieldtype;
 
-use Youshido\GraphQL\Type\Scalar\IdType;
+use Youshido\GraphqL\Execution\ResolveInfo;
+use ProcessWire\Template;
+use ProcessWire\GraphQL\Type\Object\PageArrayType;
+use ProcessWire\GraphQL\Type\Object\TemplatedPageArrayType;
 use ProcessWire\GraphQL\Field\Page\Fieldtype\AbstractFieldtype;
 
 class FieldtypePage extends AbstractFieldtype {
 
   public function getType()
   {
-    return new IdType();
+    // if template is chosen for the FieldtypePage
+    // then we resolve to TemplatedPageArrayType
+    if ($this->field->template_id) {
+      $template = \ProcessWire\wire('templates')->get($this->field->template_id);
+      if ($template instanceof Template) return new TemplatedPageArrayType($template);
+    }
+      
+    return new PageArrayType();   
+  }
+
+  public function resolve($value, array $args, ResolveInfo $info)
+  {
+    // first we turn of output formatting because we handle
+    // FeildtypePage as a PageArray/WireArray
+    $value->of(false);
+
+    // get the desired value
+    $fieldName = $this->field->name;
+    $images = $value->$fieldName;
+
+    // turn the output formatting back on so that other fieldtypes
+    // are handled properly
+    $value->of(true);
+
+    // return the value
+    return $images;
   }
 
 }
