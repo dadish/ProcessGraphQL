0c36888062022a61ba4ea7c839126fde62d7587e
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-01-26 17:21:15 +0600

    Prettify.

--- GraphiQL/partial.php
+++ GraphiQL/partial.php
@@ -1,10 +1,36 @@
 <style>
-  #graphiql {
-    height: 100vh;
+  #content.pw-content{
+    padding: 0px;
   }
   #content .pw-container, #content .container{
     width: 100%;
     max-width: none;
+  }
+  #graphiql {
+    height: 100vh;
+  }
+  #graphiql * {
+    box-sizing: content-box;
+    -webkit-box-sizing: content-box;
+    -moz-box-sizing: content-box;
+    line-height: 1rem;
+  }
+  #graphiql .doc-explorer-title{
+    overflow: hidden;
+  }
+  #graphiql .doc-explorer-contents{
+    padding: 5px 20px 15px;
+  }
+  #graphiql .graphiql-container .search-box{
+    margin: 5px 0px 8px 0;
+  }
+  #graphiql .graphiql-container .search-box:before{
+    top: 5px;
+  }
+  #graphiql .graphiql-container .search-box input{
+    padding: 6px 0px 8px 24px;
+    width: 94%;
+    background: none;
   }
   <?= $CSS ?>
 </style>
