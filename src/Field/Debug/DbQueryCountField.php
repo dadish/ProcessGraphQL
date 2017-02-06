b9eb7cd3787e1666ae509121a7b214ad9e1695bf
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-06 20:56:54 +0600

    Add descriptiotn to dbQueryCount & make it non-null.

--- src/Field/Debug/DbQueryCountField.php
+++ src/Field/Debug/DbQueryCountField.php
@@ -3,6 +3,7 @@
 namespace ProcessWire\GraphQL\Field\Debug;
 
 use Youshido\GraphQL\Field\AbstractField;
+use Youshido\GraphQL\Type\NonNullType;
 use Youshido\GraphQL\Type\Scalar\IntType;
 use Youshido\GraphQL\Execution\ResolveInfo;
 
@@ -13,9 +14,14 @@ class DBQueryCountField extends AbstractField {
     return 'dbQueryCount';
   }
 
+  public function getDescription()
+  {
+    return 'The total number of queries to database made to fulfill this request.';
+  }
+
   public function getType()
   {
-    return new IntType();
+    return new NonNullType(new IntType());
   }
 
   public function resolve($value, array $args, ResolveInfo $info)
