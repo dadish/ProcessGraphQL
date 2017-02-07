f365ae3e26a5b03de490c82e18a2a6e03a1dd954
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-07 09:03:16 +0600

    Fix the template permissions bug.

--- src/Settings.php
+++ src/Settings.php
@@ -11,14 +11,20 @@ class Settings {
 
   public static function getLegalTemplates()
   {
-    
     $legalTemplates = self::module()->legalTemplates;
     $templates = \ProcessWire\wire('templates')->find("name=" . implode('|', $legalTemplates));
-    
     $user = \ProcessWire\wire('user');
+
+    // Wierd behavior with ProcessWire. $user->hasPermission() does not
+    // work if you do not load the required roles beforehand.
+    \ProcessWire\wire('roles')->find("");
     
     foreach ($templates as $template) {
-      if (!$user->hasPermission('page-view', $template)) {
+      // we serve templates with access control disabled
+      if (!$template->useRoles) continue;
+
+      // if enabled we serve only those that user has permission to view
+      if (!$user->hasTemplatePermission('page-view', $template)) {
         $templates->remove($template);
       }
     }
