8078626c2dea25c406a81335eccceba88a475c6c
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-13 20:52:20 +0600

    Fix class import typo. Issue #2.

--- src/Field/Page/Fieldtype/FieldtypeDatetime.php
+++ src/Field/Page/Fieldtype/FieldtypeDatetime.php
@@ -2,7 +2,7 @@
 
 namespace ProcessWire\GraphQL\Field\Page\Fieldtype;
 
-use Youshido\GraphQL\Type\Scalar\DatetimeType;
+use Youshido\GraphQL\Type\Scalar\DateTimeType;
 use ProcessWire\GraphQL\Field\Page\Fieldtype\AbstractFieldtype;
 
 class FieldtypeDatetime extends AbstractFieldtype {
