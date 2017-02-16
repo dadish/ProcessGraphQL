a84669e20ea87787a9a035aee4a91a6b178eee17
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-16 12:38:00 +0600

    Give access to all legal fields to `superuser`.

--- src/Config.php
+++ src/Config.php
@@ -100,6 +100,9 @@ class Config extends WireData {
     $fields = $this->getLegalFields();
     $roles = $permission . "Roles";
 
+    // if superuser give access to everything
+    if (Utils::user()->isSuperuser()) return $fields;
+
     if (Utils::moduleConfig()->grantFieldAccess) {
       foreach ($fields as $field) {
         if ($field->useRoles && !$this->userHasRoleIn($field->$roles)) {
@@ -114,6 +117,7 @@ class Config extends WireData {
         }
       }
     }
+
     return $fields;
   }
 
