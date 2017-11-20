7f379a610148b4309b3de52a0979d463e38e65f6
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-11-20 11:42:25 +0600

    Add example test.

---
+++ test/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+	public function testPushAndPop()
+	{
+		$stack = [];
+		$this->assertEquals(0, count($stack));
+
+		array_push($stack, 'foo');
+		$this->assertEquals('foo', $stack[count($stack)-1]);
+		$this->assertEquals(1, count($stack));
+
+		$this->assertEquals('foo', array_pop($stack));
+		$this->assertEquals(0, count($stack));
+	}
+}
