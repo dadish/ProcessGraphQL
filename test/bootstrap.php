8acc4498ee3d1da64ee77c4fa0a38eb4a2dc0b1a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-11-20 14:25:13 +0600

    Implement a database fixture for testing.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,11 +1,30 @@
 <?php namespace ProcessWire\GraphQL;
 
-require_once __DIR__ . "/../vendor/autoload.php";
+// paths
+$baseDir = __DIR__ . "/../";
+$pwDir = $baseDir . "vendor/processwire/processwire/";
+$siteDir = $pwDir . "site-default";
+
+// load dependencies
+require_once $baseDir . "vendor/autoload.php";
+
+// overwrite site-default's config.php with our own custom one
+copy(__DIR__ . "/pw-config.php", $siteDir . "/config.php");
 
 use ProcessWire\ProcessWire;
 
-$config = ProcessWire::buildConfig(__DIR__ . "/../vendor/processwire/processwire");
+$config = ProcessWire::buildConfig($pwDir, null, [
+  "siteDir" => "site-default"
+]);
+
+// fill up database
+echo "Database setup started...\n";
+$dsn = "mysql:dbname=$config->dbName;host=$config->dbHost";
+$sql = \file_get_contents(__DIR__ . "/skyscrapers.sql");
+$pdo = new \PDO($dsn, $config->dbUser, $config->dbPass);
+$pdo->exec($sql);
+echo "Database setup finished\n\n";
 
 $wire = new ProcessWire($config);
 
-echo \json_encode($config);
+// echo \json_encode($config);
