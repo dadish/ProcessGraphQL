2d6979202a0b55555a30e43272d49e41f06b9c4a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-12-10 14:03:18 +0600

    Test `dbQueryCount` field.

---
+++ test/Field/Debug/DbQueryCountFieldTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Debug;
+
+use ProcessWire\GraphQL\Test\GraphQLTestCase;
+use ProcessWire\GraphQL\Utils;
+
+class DbQueryCountFieldTest extends GraphQLTestCase {
+
+  public static $debug;
+
+  public static function setUpBeforeClass()
+  {
+    self::$debug = Utils::config()->debug;
+    parent::setUpBeforeClass();
+  }
+
+  public static function tearDownAfterClass()
+  {
+    Utils::config()->debug = self::$debug;
+    parent::tearDownAfterClass();
+  }
+
+  public function testUnavailable()
+  {
+    Utils::config()->debug = false;
+    $query = '{
+      dbQueryCount
+    }';
+    $res = $this->execute($query);
+    $this->assertObjectHasAttribute('errors', $res, 'When debug turned off, `dbQueryCount` field must be unavailable.');
+  }
+
+  public function testValue()
+  {
+    Utils::config()->debug = true;
+    $query = '{
+      dbQueryCount
+    }';
+    $res = $this->execute($query);
+    $this->assertEquals(count(\ProcessWire\Database::getQueryLog()), $res->data->dbQueryCount, '`dbQueryCount` must return the number of db query logs');
+  }
+
+}
