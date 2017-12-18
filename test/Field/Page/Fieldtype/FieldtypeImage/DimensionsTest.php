dc0a0e4d576c2b46ac151eaaafd8f5bf4ef6a3b2
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-12-18 21:54:47 +0600

    Test `FieldtypeImage` width & height.

---
+++ test/Field/Page/Fieldtype/FieldtypeImage/DimensionsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use ProcessWire\GraphQL\Utils;
+use ProcessWire\GraphQL\Test\GraphQLTestCase;
+use ProcessWire\GraphQL\Test\Field\Page\Fieldtype\FieldtypeTestTrait;
+
+class FieldtypeImageDimensionsTest extends GraphQLTestCase {
+
+	const TEMPLATE_NAME = 'skyscraper';
+	const FIELD_NAME = 'images';
+	const FIELD_TYPE = 'FieldtypeImage';
+
+	use FieldtypeTestTrait;
+
+	public function testDimensions()
+	{
+		$skyscraper = Utils::pages()->find("template=skyscraper, images.count>0, sort=random")->first();
+		$query = "{
+			skyscraper (s: \"id=$skyscraper->id\") {
+				list {
+					images {
+						height
+						width
+					}
+				}
+			}
+		}";
+		$res = $this->execute($query);
+		$this->assertEquals(
+			$skyscraper->images->first()->height,
+			$res->data->skyscraper->list[0]->images[0]->height,
+			'Retrieves image height.'
+		);
+		$this->assertEquals(
+			$skyscraper->images->first()->width,
+			$res->data->skyscraper->list[0]->images[0]->width,
+			'Retrieves image width.'
+		);
+	}
+
+}
