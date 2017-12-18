10a34a765b8e7224d4ac43faf198aa81e8ccf836
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-12-18 17:37:21 +0600

    Test `FieldtypeFloat` field.

---
+++ test/Field/Page/Fieldtype/FieldtypeFloatTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Test\GraphqlTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Fieldtype\FieldtypeTestTrait;
+use \ProcessWire\GraphQL\Utils;
+
+class FieldtypeFloatTest extends GraphqlTestCase {  
+
+  const TEMPLATE_NAME = 'skyscraper';
+  const FIELD_NAME = 'height';
+  const FIELD_TYPE = 'FieldtypeFloat';
+
+  use FieldtypeTestTrait;
+
+  public function testValue()
+  {
+    $skyscraper = Utils::pages()->find("template=skyscraper, sort=random")->first();
+    $query = "{
+      skyscraper(s: \"id=$skyscraper->id\") {
+        list {
+          height
+        }
+      }
+    }";
+    $res = $this->execute($query);
+    $this->assertEquals($skyscraper->height, $res->data->skyscraper->list[0]->height, 'Retrieves field value.');
+  }
+
+}
