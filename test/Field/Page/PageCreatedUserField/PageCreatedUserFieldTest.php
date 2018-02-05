aa86b51a2b0c6542e4e3f4d2b41b072f4a5e1815
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-05 17:26:08 +0600

    Test `createdUser` field.

---
+++ test/Field/Page/PageCreatedUserField/PageCreatedUserFieldTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * If user has access to user template then
+ * createdUser returns a user Page.
+ */
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Traits\AccessTrait;
+
+class PageCreatedUserFieldTest extends GraphQLTestCase {
+
+  const accessRules = [
+    'legalTemplates' => ['skyscraper', 'user'],
+    'legalPageFields' => ['createdUser'],
+  ];
+
+  use AccessTrait;
+	
+  public function testValue()
+  {
+  	$skyscraper = Utils::pages()->get("template=skyscraper");
+  	$query = "{
+  		skyscraper (s: \"id=$skyscraper->id\") {
+  			list {
+  				createdUser {
+            name
+            email
+          }
+  			}
+  		}
+  	}";
+  	$res = $this->execute($query);
+  	$this->assertEquals($skyscraper->createdUser->name, $res->data->skyscraper->list[0]->createdUser->name, 'Retrieves `createdUser` field of the page.');
+  }
+
+}
