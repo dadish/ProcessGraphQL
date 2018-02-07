8fa1d85b91ef6d0e7f41f37524a4f15d54cd7a5c
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-07 17:53:47 +0600

    Test page `id` field.

---
+++ test/Field/Page/PageIdField/PageIdFieldTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Traits\AccessTrait;
+
+class PageIdFieldTest extends GraphQLTestCase {
+
+  const accessRules = [
+    'legalTemplates' => ['skyscraper'],
+    'legalPageFields' => ['id'],
+  ];
+
+  use AccessTrait;
+	
+  public function testValue()
+  {
+  	$skyscraper = Utils::pages()->get("template=skyscraper");
+  	$query = "{
+  		skyscraper (s: \"id=$skyscraper->id\") {
+  			list {
+  				id
+  			}
+  		}
+  	}";
+  	$res = $this->execute($query);
+  	$this->assertEquals($skyscraper->id, $res->data->skyscraper->list[0]->id, 'Retrieves `id` field of the page.');
+  }
+
+}
