030d371c6b2586c59d44001d1c88d39928d80d1e
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-09 11:57:54 +0600

    Test page `name` field.

---
+++ test/Field/Page/PageNameField/PageNameFieldTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Traits\AccessTrait;
+
+class PageNameFieldTest extends GraphQLTestCase {
+
+  const accessRules = [
+    'legalTemplates' => ['skyscraper'],
+    'legalPageFields' => ['name'],
+  ];
+
+  use AccessTrait;
+	
+  public function testValue()
+  {
+  	$skyscraper = Utils::pages()->get("template=skyscraper");
+  	$query = "{
+  		skyscraper (s: \"id=$skyscraper->id\") {
+  			list {
+  				name
+  			}
+  		}
+  	}";
+  	$res = $this->execute($query);
+  	$this->assertEquals($skyscraper->name, $res->data->skyscraper->list[0]->name, 'Retrieves `name` field of the page.');
+  }
+
+}
