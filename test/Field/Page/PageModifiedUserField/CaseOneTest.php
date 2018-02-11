8ab37e33027aede428246b6ea42d98ad48bffbac
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-11 18:29:57 +0600

    Test page `parent` field.

--- test/Field/Page/PageModifiedUserField/CaseOneTest.php
+++ test/Field/Page/PageModifiedUserField/CaseOneTest.php
@@ -28,7 +28,6 @@ class PageModifiedUserFieldCaseOneTest extends GraphQLTestCase {
   			list {
   				modifiedUser {
             name
-            email
           }
   			}
   		}
---
+++ test/Field/Page/PageParentField/CaseOneTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * When user got access to both requested page template
+ * and it's parent's template. The `parent` field returns
+ * the parent page.
+ */
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Traits\AccessTrait;
+
+class PageParentFieldCaseOneTest extends GraphQLTestCase {
+
+  const accessRules = [
+    'legalTemplates' => ['city', 'skyscraper'],
+    'legalPageFields' => ['parent', 'name'],
+  ];
+
+  use AccessTrait;
+  
+  public function testValue()
+  {
+    $skyscraper = Utils::pages()->get("template=skyscraper");
+    $query = "{
+      skyscraper (s: \"id=$skyscraper->id\") {
+        list {
+          parent {
+            name
+          }
+        }
+      }
+    }";
+    $res = $this->execute($query);
+    $this->assertEquals($skyscraper->parent->name, $res->data->skyscraper->list[0]->parent->name, 'Retrieves parent page.');
+  }
+
+}
