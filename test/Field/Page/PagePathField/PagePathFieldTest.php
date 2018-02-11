d7d3c54e91051210bb75d3d20a67bc11c2bbaaff
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-11 19:47:48 +0600

    Test page `path` field.

---
+++ test/Field/Page/PagePathField/PagePathFieldTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Traits\AccessTrait;
+
+class PagePathFieldTest extends GraphQLTestCase {
+
+  const accessRules = [
+    'legalTemplates' => ['skyscraper'],
+    'legalPageFields' => ['path'],
+  ];
+
+  use AccessTrait;
+	
+  public function testValue()
+  {
+  	$skyscraper = Utils::pages()->get("template=skyscraper");
+  	$query = "{
+  		skyscraper (s: \"id=$skyscraper->id\") {
+  			list {
+  				path
+  			}
+  		}
+  	}";
+  	$res = $this->execute($query);
+  	$this->assertEquals($skyscraper->path, $res->data->skyscraper->list[0]->path, 'Retrieves `path` field of the page.');
+  }
+
+}
