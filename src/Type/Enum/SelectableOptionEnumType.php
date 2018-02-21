3f7558d6cd87b847776563d47205cd4c5f1306c7
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2018-02-21 15:32:33 +0600

    Add support for SelectableOption input.

---
+++ src/Type/Enum/SelectableOptionEnumType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProcessWire\GraphQL\Type\Enum;
+
+use Youshido\GraphQL\Type\Enum\AbstractEnumType;
+use ProcessWire\Field;
+
+class SelectableOptionEnumType extends AbstractEnumType {
+
+	function __construct(Field $field)
+	{
+		$this->field = $field;
+		parent::__construct([]);
+	}
+
+	public function getValues()
+	{
+		$options = [];
+		foreach ($this->field->type->getOptions($this->field) as $option) {
+			$options[] = [
+				'value' => $option->id,
+				'name' => $option->value | $option->title,
+			];
+		}
+		return $options;
+	}
+
+}
