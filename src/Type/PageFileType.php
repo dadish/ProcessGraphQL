83856d72409c169f486fdfb876e0aef860cc158e
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-04-11 16:10:35 +0600

    Create PageFileType.

---
+++ src/Type/PageFileType.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace ProcessWire\GraphQL\Type;
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+class PageFileType {
+
+  public static $name = 'PageFile';
+
+  public static $description = 'ProcessWire PageFile.';
+
+  private static $type;
+
+  public static function type()
+  {
+    if (self::$type) {
+      return self::$type;
+    }
+
+    self::$type = new ObjectType([
+      'name' => self::$name,
+      'description' => self::$description,
+      'fields' => function () {
+        return self::getFields();
+      }
+    ]);
+
+    return self::$type;
+  }
+
+  public static function getFields()
+  {
+    return [
+      [
+        'name' => 'url',
+        'type' => Type::string(),
+        'description' => 'URL to the file on the server.',
+        'resolve' => function ($value) {
+          return (string) $value->url;
+        }
+      ],
+
+      [
+        'name' => 'httpUrl',
+        'type' => Type::string(),
+        'description' => 'The web accessible URL (with scheme and hostname) to this Pagefile.',
+        'resolve' => function ($value) {
+          return (string) $value->httpUrl;
+        }
+      ],
+
+      [
+        'name' => 'URL',
+        'type' => Type::string(),
+        'description' => "Same as 'url' property but with browser cache busting query
+                          string appended that represents the file's modification time.",
+        'resolve' => function ($value) {
+          return (string) $value->URL;
+        }
+      ],
+
+      [
+        'name' => 'basename',
+        'type' => Type::string(),
+        'description' => 'The filename without the path.',
+        'resolve' => function ($value) {
+          return (string) $value->basename;
+        }
+      ],
+
+      [
+        'name' => 'description',
+        'type' => Type::string(),
+        'description' => 'The description of the file.',
+        'resolve' => function ($value) {
+          return (string) $value->description;
+        }
+      ],
+
+      [
+        'name' => 'ext',
+        'type' => Type::string(),
+        'description' => "File’s extension.",
+        'resolve' => function ($value) {
+          return (string) $value->ext;
+        }
+      ],
+
+      [
+        'name' => 'filesize',
+        'type' => Type::int(),
+        'description' => 'File size (number of bytes).',
+        'resolve' => function ($value) {
+          return (integer) $value->filesize;
+        }
+      ],
+
+      [
+        'name' => 'modified',
+        'type' => Type::int(),
+        'description' => 'Unix timestamp of when Pagefile (file, description or tags) was last modified.',
+        'resolve' => function ($value) {
+          return (integer) $value->modified;
+        }
+      ],
+
+      [
+        'name' => 'mtime',
+        'type' => Type::int(),
+        'description' => 'Unix timestamp of when file (only) was last modified.',
+        'resolve' => function ($value) {
+          return (integer) $value->mtime;
+        }
+      ],
+
+      [
+        'name' => 'created',
+        'type' => Type::int(),
+        'description' => 'Unix timestamp of when file was created.',
+        'resolve' => function ($value) {
+          return (integer) $value->created;
+        }
+      ],
+
+      [
+        'name' => 'filesizeStr',
+        'type' => Type::string(),
+        'description' => 'File size as a formatted string, i.e. “123 Kb”.',
+        'resolve' => function ($value) {
+          return (string) $value->filesizeStr;
+        }
+      ],
+    ];
+  }
+}
