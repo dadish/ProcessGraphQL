094b2b76289ae61b8c9a9e87d198217b75d01181
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-07-27 17:55:53 +0600

    Enable test for FieldtypeInteger.

---
+++ src/Type/Fieldtype/FieldtypeInteger.php
@@ -0,0 +1,22 @@
+<?php namespace ProcessWire\GraphQL\Type\Fieldtype;
+
+use GraphQL\Type\Definition\Type;
+use ProcessWire\GraphQL\Type\CacheTrait;
+
+class FieldtypeInteger
+{ 
+  use CacheTrait;
+  public static function type()
+  {
+    return Type::int();
+  }
+
+  public static function field($options)
+  {
+    return self::cache('field-' . $options['name'], function () use ($options) {
+      return array_merge($options, [
+        'type' => self::type(),
+      ]);
+    });
+  }
+}
--- test/Field/Page/Fieldtype/FieldtypeInteger/FieldtypeIntegerTest.php
+++ test/Field/Page/Fieldtype/FieldtypeInteger/FieldtypeIntegerTest.php
@@ -29,7 +29,7 @@ class FieldtypeIntegerTest extends GraphqlTestCase {
         }
       }
     }";
-    $res = $this->execute($query);
+    $res = self::execute($query);
     $this->assertEquals($skyscraper->floors, $res->data->skyscraper->list[0]->floors, 'Retrieves field value.');
   }
 
