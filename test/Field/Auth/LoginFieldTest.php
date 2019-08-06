cf43455cb4d3feb26464a1b966a8ae724ef4ff49
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-08-06 17:20:31 +0600

    Fix login tests.

--- test/Field/Auth/LoginFieldTest.php
+++ test/Field/Auth/LoginFieldTest.php
@@ -25,7 +25,7 @@ class LoginFieldTest extends GraphQLTestCase {
     $config = Utils::config();
     $pass = $config->testUsers['admin'];
     $query = "{
-      login(username:\"admin\", password:\"$pass\") {
+      login(name:\"admin\", pass:\"$pass\") {
         statusCode
         message
       }
@@ -39,7 +39,7 @@ class LoginFieldTest extends GraphQLTestCase {
     $config = Utils::config();
     $pass = 'some-random-stuff';
     $query = "{
-      login(username:\"admin\", password:\"$pass\") {
+      login(name:\"admin\", pass:\"$pass\") {
         statusCode
         message
       }
