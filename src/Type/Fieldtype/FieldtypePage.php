1337cde0ea048a5994f62ff3b0130501e71d6612
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-08-18 21:49:29 +0600

    Implement field, inputField & setValue methods for FieldtypePage.

--- src/Type/Fieldtype/FieldtypePage.php
+++ src/Type/Fieldtype/FieldtypePage.php
@@ -1,17 +1,13 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
+use GraphQL\Type\Definition\Type;
+use ProcessWire\FieldtypePage as PWFieldtypePage;
 use ProcessWire\GraphQL\Type\PageArrayType;
 use ProcessWire\GraphQL\Type\Traits\CacheTrait;
-use ProcessWire\GraphQL\Type\Traits\FieldTrait;
-use ProcessWire\GraphQL\Type\Traits\InputFieldTrait;
-use ProcessWire\GraphQL\Type\Traits\SetValueTrait;
 
 class FieldtypePage
 {
   use CacheTrait;
-  use FieldTrait;
-  use InputFieldTrait;
-  use SetValueTrait;
   public static function type($field)
   {
     $template = null;
@@ -22,5 +18,39 @@ class FieldtypePage
     }
       
     return PageArrayType::type($template);
+  }
+
+  public static function field($field)
+  {
+    return self::cache("field-{$field->name}", function () use ($field) {
+      // description
+      $desc = $field->description;
+      if (!$desc) {
+        $desc = "Field with the type of {$field->type}";
+      }
+
+      return [
+        'name' => $field->name,
+        'description' => $desc,
+        'type' => $field->required ? Type::nonNull(self::type($field)) : self::type($field),
+        'resolve' => function ($value) use ($field) {
+          $fieldName = $field->name;
+          $field = \ProcessWire\wire('fields')->get($fieldName);
+          $field->derefAsPage = PWFieldtypePage::derefAsPageArray;
+          return $value->$fieldName;
+        }
+      ];
+    });
+  }
+
+  public static function inputField()
+  {
+    return Type::listOf(Type::string());
+  }
+
+  public function setValue(Page $page, $field, $value)
+  {
+    $fieldName = $field->name;
+    $page->$fieldName = implode('|', $value);
   }
 }
