589c24ab395f2dd56a8d454203a5997fffdf6fab
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-11 01:38:32 +0500

    Setup debugging environment.

--- test/client.php
+++ test/client.php
@@ -18,62 +18,31 @@ $wire = new ProcessWire($wireConfig);
 // =====================
 $config = $wire->fuel('config');
 
-$scripts = scandir(realpath(__DIR__  . "/../GraphiQL/build/static/js"));
-$scripts = array_walk($scripts, function ($filename) use ($config) {
-  if (preg_match('/^main\..*\.js$/', $filename)) {
-    $config->scripts->add(realpath(__DIR__ . "/../GraphiQL/build/static/js/{$filename}"));
-  }
-});
-
-$styles = scandir(realpath(__DIR__  . "/../GraphiQL/build/static/css"));
-$styles = array_walk($styles, function ($filename) use ($config) {
-  if (preg_match('/^main\..*\.css$/', $filename)) {
-    $config->styles->add(realpath(__DIR__ . "/../GraphiQL/build/static/css/{$filename}"));
-  }
-});
-
 $config->js('ProcessGraphQL', [
   'GraphQLServerUrl' => 'http://localhost:8081',
 ]);
 
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/favicon.ico">
-    <title>GraphiQL</title>
-    <style>
-      body {
-        height: 100%;
-        margin: 0;
-        width: 100%;
-        overflow: hidden;
-      }
-      #graphiql {
-        height: 100vh;
-      }
-    </style>
-  </head>
-  <body>
-    <style>
-      <?php
-        foreach($config->styles as $file) {
-          require_once($file);
-          echo "\n\n";
-        }
-      ?>
-    </style>
-    <div id="graphiql">Loading...</div>
-    <script>
-      var config = <?= json_encode($config->js()) ?>;
-      <?php
-        foreach($config->scripts as $file) {
-          require_once($file);
-          echo ";;\n";
-        }
-      ?>
-    </script>
-  </body>
-</html>
+$config->scripts->add("https://cdn.jsdelivr.net/es6-promise/4.0.5/es6-promise.auto.min.js");
+$config->scripts->add("https://cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js");
+$config->scripts->add("https://cdn.jsdelivr.net/react/15.4.2/react.min.js");
+$config->scripts->add("https://cdn.jsdelivr.net/react/15.4.2/react-dom.min.js");
+$config->scripts->add('GraphiQL/node_modules/graphiql/graphiql.min.js');
+$config->styles->add('GraphiQL/node_modules/graphiql/graphiql.css');
+
+$filename = realpath($baseDir . $_SERVER['SCRIPT_NAME']);
+if ($filename === $baseDir) {
+  require_once(realpath("$baseDir/GraphiQL/full.php"));
+}
+
+if (!file_exists($filename)) {
+  http_response_code(404);
+  echo "Not Found!";
+  return;
+}
+
+if (preg_match('/^[a-zA-Z\/_.]+\.js$/', $filename)) {
+  header('Content-type: text/javascript');
+} else if (preg_match('/^[a-zA-Z\/_.]+\.css$/', $filename)) {
+  header('Content-type: text/css');
+}
+require_once($filename);
