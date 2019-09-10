1ef14f9c556c31239768bc630d5e5ba46da4b0fb
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-10 15:25:02 +0500

    Add styles.

--- GraphiQL/src/index.js
+++ GraphiQL/src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   return element;
 }
