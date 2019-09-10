9447c2764bff7dc66728f2cf0988ed909360ba18
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-10 15:01:41 +0500

    New GraphiQL directory structure.

--- GraphiQL/src/index.js
+++ GraphiQL/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import _ from 'lodash';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function component() {
+  const element = document.createElement('div');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ GraphiQL/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader',
+          'style-loader',
+        ]
+      }
+    ]
+  }
+};
