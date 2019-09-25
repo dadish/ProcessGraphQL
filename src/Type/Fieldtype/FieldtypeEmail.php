f833e7726ccf47d7bf3ced31b2ca7604414ca73f
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-25 11:10:06 +0500

    Update caching for FieldtypeEmail.

--- src/Type/Fieldtype/FieldtypeEmail.php
+++ src/Type/Fieldtype/FieldtypeEmail.php
@@ -1,23 +1,27 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
 use GraphQL\Type\Definition\CustomScalarType;
-use ProcessWire\GraphQL\Type\Traits\CacheTrait;
+use ProcessWire\GraphQL\Cache;
 use ProcessWire\GraphQL\Type\Traits\FieldTrait;
 use ProcessWire\GraphQL\Type\Traits\InputFieldTrait;
 use ProcessWire\GraphQL\Type\Traits\SetValueTrait;
 
 class FieldtypeEmail
 { 
-  use CacheTrait;
   use FieldTrait;
   use InputFieldTrait;
   use SetValueTrait;
+
+  public static $name = 'Email';
+
+  public static $description = 'E-Mail address in valid format';
+
   public static function type($field)
   {
-    return self::cache($field->name, function () {
+    return Cache::type($field->name, function () {
       return new CustomScalarType([
-        'name' => 'Email',
-        'description' => 'E-Mail address in valid format',
+        'name' => self::$name,
+        'description' => self::$description,
         'serialize' => function ($value) {
           return (string) $value;
         },
