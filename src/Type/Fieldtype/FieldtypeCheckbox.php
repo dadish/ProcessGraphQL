562489712690c7c65d308010e757fba86e7f82df
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-25 10:53:30 +0500

    Update caching for FieldtypeCheckbox.

--- src/Type/Fieldtype/FieldtypeCheckbox.php
+++ src/Type/Fieldtype/FieldtypeCheckbox.php
@@ -1,23 +1,27 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
 use GraphQL\Type\Definition\CustomScalarType;
-use ProcessWire\GraphQL\Type\Traits\CacheTrait;
+use ProcessWire\GraphQL\Cache;
 use ProcessWire\GraphQL\Type\Traits\FieldTrait;
 use ProcessWire\GraphQL\Type\Traits\InputFieldTrait;
 use ProcessWire\GraphQL\Type\Traits\SetValueTrait;
 
 class FieldtypeCheckbox
 { 
-  use CacheTrait;
   use FieldTrait;
   use InputFieldTrait;
   use SetValueTrait;
-  public static function type($field)
+  
+  public static $name = 'Checkbox';
+
+  public static $description = 'An ON/OFF toggle via a single checkbox.';
+
+  public static function type()
   {
-    return self::cache($field->name, function () {
+    return Cache::type(self::$name, function () {
       return new CustomScalarType([
-        'name' => 'Checkbox',
-        'description' => 'An ON/OFF toggle via a single checkbox.',
+        'name' => self::$name,
+        'description' => self::$description,
         'serialize' => function ($value) {
           return (boolean) $value;
         },
