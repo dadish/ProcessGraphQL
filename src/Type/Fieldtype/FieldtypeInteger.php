5b5c9f4c06c4f94a271bed258497e905f9513709
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-25 11:19:36 +0500

    Update caching for FieldtypeInteger.

--- src/Type/Fieldtype/FieldtypeInteger.php
+++ src/Type/Fieldtype/FieldtypeInteger.php
@@ -1,14 +1,12 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
 use GraphQL\Type\Definition\Type;
-use ProcessWire\GraphQL\Type\Traits\CacheTrait;
 use ProcessWire\GraphQL\Type\Traits\FieldTrait;
 use ProcessWire\GraphQL\Type\Traits\InputFieldTrait;
 use ProcessWire\GraphQL\Type\Traits\SetValueTrait;
 
 class FieldtypeInteger
 { 
-  use CacheTrait;
   use FieldTrait;
   use InputFieldTrait;
   use SetValueTrait;
