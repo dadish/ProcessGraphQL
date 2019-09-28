14678210355d803e3e9eaa6284256c7cb0b5a2d6
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-09-28 21:52:47 +0500

    Update release script.

---
+++ scripts/commons.js
@@ -0,0 +1,71 @@
+const execa = require("execa");
+const fs = require("fs-extra");
+const ora = require("ora");
+
+const extraneousFiles = [
+  "bin",
+  "imgs",
+  "test",
+  "vendor",
+  ".travis.yml",
+  "Changelog.md",
+  "composer.lock",
+  "package-lock.json",
+  "ScreenCast.md",
+  "Todo.md",
+  ".gitignore",
+  "GraphiQL/package.json",
+  "GraphiQL/package-lock.json"
+];
+
+const vendorExtraneousFiles = [
+  "vendor/webonyx/graphql-php/docs",
+  "vendor/webonyx/graphql-php/examples",
+  "vendor/webonyx/graphql-php/.scrutinizer.yml",
+  "vendor/webonyx/graphql-php/CHANGELOG.md",
+  "vendor/webonyx/graphql-php/composer.json",
+  "vendor/webonyx/graphql-php/LICENSE",
+  "vendor/webonyx/graphql-php/phpcs.xml.dist",
+  "vendor/webonyx/graphql-php/phpstan.neon.dist",
+  "vendor/webonyx/graphql-php/README.md",
+  "vendor/webonyx/graphql-php/UPGRADE.md"
+];
+
+const fakeSpinner = {
+  start: () => {},
+  succeed: () => {},
+  fail: () => {}
+};
+
+async function updateFile(filename, matcher, replaceStr, message) {
+  const spinner = message ? ora(message) : fakeSpinner;
+  try {
+    spinner.start();
+    let content = await fs.readFile(filename, "utf8");
+    content = content.replace(matcher, replaceStr);
+    await fs.writeFile(filename, content);
+    spinner.succeed();
+  } catch (err) {
+    spinner.fail();
+    throw new Error(err);
+  }
+}
+
+async function execute(file, arguments, message) {
+  const spinner = message ? ora(message) : fakeSpinner;
+  try {
+    spinner.start();
+    await execa(file, arguments);
+    spinner.succeed();
+  } catch (err) {
+    spinner.fail();
+    throw new Error(err);
+  }
+}
+
+module.exports = {
+  extraneousFiles,
+  vendorExtraneousFiles,
+  updateFile,
+  execute
+};
--- scripts/release.js
+++ scripts/release.js
@@ -1,253 +1,130 @@
-#! /usr/bin/env node
-const fs = require("fs");
 const path = require("path");
-const shell = require("shelljs");
-const ora = require("ora");
+const {
+  extraneousFiles,
+  vendorExtraneousFiles,
+  execute,
+  updateFile
+} = require("./commons");
 
-const silent = { silent: true };
-const RELEASE_BRANCH_NAME = "release";
-const MASTER_BRANCH_NAME = "master";
+async function release(releaseLevel) {
+  // create the release branch
+  await execute("git", ["branch", "release"], "Creating the release branch");
 
-const RELEASE_TEST = "test";
-let spinner;
+  // switch to the release branch
+  await execute(
+    "git",
+    ["checkout", "release"],
+    "Switching to the release branch"
+  );
+
+  // remove files that we don't need in the production code
+  await execute("rm", ["-rf", ...extraneousFiles], "Removing extraneous files");
+
+  // we removed all the vendor code in the previous step
+  // now we install it again, but only the production dependencies.
+  await execute(
+    "composer",
+    ["install", "--no-dev"],
+    "Installing php dependencies (--no-dev)"
+  );
+
+  // remove verdor's files that we don't use in production code
+  await execute(
+    "rm",
+    ["-rf", ...vendorExtraneousFiles],
+    "Removing extraneous files from graphql-php"
+  );
+
+  // copy the graphiql dependencies to our GraphiQL folder
+  await execute(
+    "cp",
+    [
+      path.resolve(`${__dirname}/../node_modules/graphiql/graphiql.min.js`),
+      path.resolve(`${__dirname}/../node_modules/graphiql/graphiql.css`),
+      path.resolve(`${__dirname}/../GraphiQL/`)
+    ],
+    "Extracting the graphiql dependencies from node_modules"
+  );
+
+  // update the .module file version number.
+  await updateFile(
+    path.resolve(`${__dirname}/../ProcessGraphQL.module`),
+    /\'version\' => \'\d+\.\d+\.\d+(-rc\d+)?\'/,
+    `'version' => '${releaseLevel}'`,
+    "Update version in ProcessGraphQL.module file."
+  );
+
+  // add changes to git stage
+  await execute("git", ["add", "."], "");
+
+  // remove node_modules from git stage
+  await execute("git", ["reset", "node_modules"], "");
+
+  // commit whatever on git stage
+  await execute("git", [
+    "commit",
+    "-m",
+    "Remove extraneous files for release."
+  ]);
+
+  // version tag
+  await execute("npm", ["version", releaseLevel], "Tagging the release.");
+
+  // switch back to master
+  await execute(
+    "git",
+    ["checkout", "master"],
+    "Switching back to master branch."
+  );
+
+  // delete the master branch
+  await execute(
+    "git",
+    ["branch", "-D", "release"],
+    "Deleting the release branch"
+  );
+
+  // install all deps back
+  await execute("composer", ["install"], "Install all vendor deps back.");
+
+  // if it's a test release then don't change anything on master branch
+  if (releaseLevel === "test") {
+    return;
+  }
+
+  // update package.json version
+  await execute(
+    "npm",
+    ["version", releaseLevel, "--no-git-tag-version"],
+    "Incrementing the package version on the master branch"
+  );
+
+  // update the .module file version number on master branch
+  await updateFile(
+    path.resolve(__dirname + "/../ProcessGraphQL.module"),
+    /\'version\' => \'\d+\.\d+\.\d+(-rc\d+)?\'/,
+    `'version' => '${releaseLevel}'`,
+    "Update version in ProcessGraphQL.module file."
+  );
+
+  // commit version update
+  await execute(
+    "git",
+    ["commit", "--all", "-m", releaseLevel],
+    "Committing package version update on master branch."
+  );
+}
 
 const releaseLevel = process.argv[2];
 if (!releaseLevel) {
-  shell.echo(
+  console.error(
     "Error: Should provide a version argument. See `npm help version`."
   );
-  shell.exit(1);
-}
-
-// create the release branch
-spinner = ora(`Creating ${RELEASE_BRANCH_NAME} branch`).start();
-const releaseBranchCreate = shell.exec(
-  `git branch ${RELEASE_BRANCH_NAME}`,
-  silent
-);
-if (releaseBranchCreate.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(releaseBranchCreate.stderr || releaseBranchCreate.stdout);
-  shell.exit(1);
-}
-
-// checkout the release branch
-spinner = ora(`Switching to the ${RELEASE_BRANCH_NAME} branch`).start();
-const releaseBranchCheckout = shell.exec(
-  `git checkout ${RELEASE_BRANCH_NAME}`,
-  silent
-);
-if (releaseBranchCheckout.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(releaseBranchCheckout.stderr || releaseBranchCheckout.stdout);
-  shell.exit(1);
-}
-
-// remove the unwanted files
-spinner = ora("Removing extraneous files").start();
-const removeDirs = shell.rm(
-  "-rf",
-  [
-    "bin",
-    "imgs",
-    "test",
-    "vendor",
-    ".travis.yml",
-    "Changelog.md",
-    "composer.lock",
-    "package-lock.json",
-    "ScreenCast.md",
-    "Todo.md",
-    ".gitignore",
-    "GraphiQL/package.json",
-    "GraphiQL/package-lock.json"
-  ],
-  silent
-);
-if (removeDirs.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(removeDirs.stderr || removeDirs.stdout);
-  shell.exit(1);
-}
-
-// install vendor dependencies
-spinner = ora("Installing php dependencies (--no-dev)").start();
-const vendorInstall = shell.exec("composer install --no-dev", silent);
-if (vendorInstall.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(vendorInstall.stderr || vendorInstall.stdout);
-  shell.exit(1);
-}
-
-// remove vendor extraneous files
-spinner = ora("Removing extraneous files from vendor code").start();
-const vendorRemoveFiles = shell.rm("-rf", [
-  "vendor/webonyx/graphql-php/docs",
-  "vendor/webonyx/graphql-php/examples",
-  "vendor/webonyx/graphql-php/.scrutinizer.yml",
-  "vendor/webonyx/graphql-php/CHANGELOG.md",
-  "vendor/webonyx/graphql-php/composer.json",
-  "vendor/webonyx/graphql-php/LICENSE",
-  "vendor/webonyx/graphql-php/phpcs.xml.dist",
-  "vendor/webonyx/graphql-php/phpstan.neon.dist",
-  "vendor/webonyx/graphql-php/README.md",
-  "vendor/webonyx/graphql-php/UPGRADE.md"
-]);
-if (vendorRemoveFiles.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(vendorRemoveFiles.stderr || vendorRemoveFiles.stdout);
-  console.log(vendorRemoveFiles.stderr);
-  shell.exit(1);
-}
-
-// extract graphiql library
-spinner = ora("Extracting the graphiql dependencies from node_modules").start();
-const extractGraphiQL = shell.cp(
-  path.resolve(`${__dirname}/../node_modules/graphiql/graphiql.min.js`),
-  path.resolve(`${__dirname}/../node_modules/graphiql/graphiql.css`),
-  path.resolve(`${__dirname}/../GraphiQL/`)
-);
-if (extractGraphiQL.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(extractGraphiQL.stderr || extractGraphiQL.stdout);
-  shell.exit(1);
+  process.exit(1);
 }
 
 try {
-  // update version in ProcessGraphQL.module file.
-  const matcher = /\'version\' => \'\d+\.\d+\.\d+(-rc\d+)?\'/;
-  const moduleFilename = path.resolve(__dirname + "/../ProcessGraphQL.module");
-  let content = fs.readFileSync(moduleFilename, "utf8");
-  content = content.replace(matcher, `'version' => '${releaseLevel}'`);
-  fs.writeFileSync(moduleFilename, content);
-  spinner.succeed();
+  release(releaseLevel);
 } catch (err) {
-  spinner.fail(
-    incrementPackageVersion.stderr || incrementPackageVersion.stdout
-  );
-  shell.exit(1);
-}
-
-// add all changes to stage
-spinner = ora("Git adding the changes").start();
-const stageChanges = shell.exec("git add . && git reset node_modules", silent);
-if (stageChanges.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(stageChanges.stderr || stageChanges.stdout);
-  shell.exit(1);
-}
-
-// commit all changes
-spinner = ora("Git committing the changes").start();
-const commitChanges = shell.exec(
-  "git commit -m 'Remove extraneous files for release.'",
-  silent
-);
-if (commitChanges.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(commitChanges.stderr || commitChanges.stdout);
-  shell.exit(1);
-}
-
-if (releaseLevel === RELEASE_TEST) {
-  // snapshot the release in "test" branch
-  spinner = ora("Snapshotting the release").start();
-  const releaseSnapshotting = shell.exec(`git branch ${RELEASE_TEST}`);
-  if (releaseSnapshotting.code === 0) {
-    spinner.succeed();
-  } else {
-    spiiner.fail(releaseSnapshotting.stderr || releaseSnapshotting.stdout);
-    shell.exit(1);
-  }
-} else {
-  // tag the release
-  spinner = ora("Tagging the release").start();
-  const releaseTagging = shell.exec(`npm version ${releaseLevel}`, silent);
-  if (releaseTagging.code === 0) {
-    spinner.succeed();
-  } else {
-    spinner.fail(releaseTagging.stderr || releaseTagging.stdout);
-    shell.exit(1);
-  }
-}
-
-// checkout the master branch
-spinner = ora(`Switching to the ${MASTER_BRANCH_NAME} branch`);
-const masterCheckout = shell.exec(`git checkout ${MASTER_BRANCH_NAME}`, silent);
-if (masterCheckout.code === 0) {
-  spinner.succeed();
-} else {
-  spiiner.fail(masterCheckout.stderr || masterCheckout.stdout);
-  shell.exit(1);
-}
-
-// delete the release branch
-spinner = ora(`Deleting the ${RELEASE_BRANCH_NAME} branch`).start();
-const releaseBranchDelete = shell.exec(
-  `git branch -D ${RELEASE_BRANCH_NAME}`,
-  silent
-);
-if (releaseBranchDelete.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(releaseBranchDelete.stderr || releaseBranchDelete.stdout);
-  shell.exit(1);
-}
-
-// install vendor deps back
-spinner = ora("Installing vendor deps back").start();
-const vendorInstallAll = shell.exec("composer install", silent);
-if (vendorInstallAll.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(vendorInstallAll.stderr || vendorInstallAll.stdout);
-  shell.exit(1);
-}
-
-// if it was a test release then do nothing on master branch
-if (releaseLevel === RELEASE_TEST) {
-  return;
-}
-
-// update version in package.json file
-// for master branch, since changes in release
-// branch do not affect master branch, the package
-// version in package.json file in master branch is old
-spinner = ora(
-  `Incrementing the package version on the ${MASTER_BRANCH_NAME} branch`
-).start();
-const incrementPackageVersion = shell.exec(
-  `npm version ${releaseLevel} --no-git-tag-version`,
-  silent
-);
-if (incrementPackageVersion.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(
-    incrementPackageVersion.stderr || incrementPackageVersion.stdout
-  );
-  shell.exit(1);
-}
-
-// commit package version change on master branch
-spinner = ora(
-  `Committing package version update on ${MASTER_BRANCH_NAME} branch`
-).start();
-const packageVersionCommit = shell.exec(
-  `git commit --all -m "${releaseLevel}"`,
-  silent
-);
-if (packageVersionCommit.code === 0) {
-  spinner.succeed();
-} else {
-  spinner.fail(packageVersionCommit.stderr || packageVersionCommit.stdout);
-  shell.exit(1);
+  console.error(err);
 }
