66049db870cbb3e5c857b116f0a8f195f1e31a7f
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-10-12 01:08:57 +0500

    Add filter support for Repeaters.

--- src/Type/Fieldtype/FieldtypeRepeater.php
+++ src/Type/Fieldtype/FieldtypeRepeater.php
@@ -1,16 +1,17 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
 use ProcessWire\Page;
+use ProcessWire\Selectors;
 use ProcessWire\GraphQL\Type\Inputfield\InputfieldRepeater;
+use ProcessWire\GraphQL\Cache;
 use ProcessWire\GraphQL\Utils;
 use ProcessWire\GraphQL\Type\PageArrayType;
-use ProcessWire\GraphQL\Type\Traits\FieldTrait;
+use ProcessWire\GraphQL\Type\SelectorType;
 use ProcessWire\GraphQL\Type\Traits\InputFieldTrait;
 use ProcessWire\GraphQL\Type\Traits\SetValueTrait;
 
 class FieldtypeRepeater
-{ 
-  use FieldTrait;
+{
   use InputFieldTrait;
   use SetValueTrait;
 
@@ -23,6 +24,42 @@ class FieldtypeRepeater
     $templateId = $field->get('template_id');
     $template = Utils::templates()->get($templateId);
     return PageArrayType::type($template);
+  }
+
+  public static function &field($field)
+  {
+    $field =& Cache::field($field->name, function () use ($field) {
+      // description
+      $desc = $field->description;
+      if (!$desc) {
+        $desc = "Field with the type of {$field->type}";
+      }
+
+      return [
+        'name' => $field->name,
+        'description' => $desc,
+        'type' => self::type($field),
+        'args' => [
+          's' => [
+            'type' => SelectorType::type(),
+            'description' => "ProcessWire selector."
+          ],
+        ],
+        'resolve' => function (Page $page, array $args) use ($field) {
+          $fieldName = $field->name;
+          $selector = "";
+          if (isset($args['s'])) {
+            $selector = $args['s'];
+          }
+          $selector = new Selectors($selector);
+          $result = $page->$fieldName->find((string) $selector);
+          if ($result instanceof NullPage) return null;
+          return $result;
+        }
+      ];
+    });
+
+    return $field;
   }
 
   public static function inputField($field)
