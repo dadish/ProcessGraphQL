4cd019e64155b6a6db829fd9412f6a93055cf2bd
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-11-23 17:15:31 +0500

    Update performance test with data validation.

--- test/Performance/DBQueryCount.php
+++ test/Performance/DBQueryCount.php
@@ -3,6 +3,7 @@
 namespace ProcessWire\GraphQL\Test;
 
 use ProcessWire\GraphQL\Test\GraphQLTestCase;
+use ProcessWire\GraphQL\Utils;
 
 class DbQueryCountTest extends GraphQLTestCase {
 
@@ -16,6 +17,7 @@ class DbQueryCountTest extends GraphQLTestCase {
         'architects', 'born', 'resume',
         'height', 'floors', 'year'
       ],
+      'maxLimit' => 1000,
     ];
   }
 
@@ -24,7 +26,7 @@ class DbQueryCountTest extends GraphQLTestCase {
    *
    * @var integer
    */
-  public static $bestQueryCount = 127;
+  public static $bestQueryCount = 180;
 
   /**
    * The query we use to test the performance of the module by counting
@@ -32,40 +34,45 @@ class DbQueryCountTest extends GraphQLTestCase {
    *
    * @var string
    */
-  public static $performanceQuery = '{
-    skyscraper(s: "architects.count>1") {
-      getTotal
-      list{
-        id
-        title
-        height
-        floors
-        year
-        images {
-          url
-          width
+  public static $performanceQuery = [
+    'query' => 'query getSkyscrapers($selector: Selector!) {
+      skyscraper(s: $selector) {
+        getTotal
+        list{
+          id
+          title
           height
-          description
-        }
-        architects{
-          list{
-            id
-            title
-            created
-            born
-            resume{
-              url
-              description
+          floors
+          year
+          images {
+            url
+            width
+            height
+            description
+          }
+          architects{
+            list{
+              id
+              title
+              created
+              born
+              resume{
+                url
+                description
+              }
             }
           }
         }
       }
-    }
-  }';
+    }',
+    'variables' => [
+      'selector' => 'architects.count>1'
+    ]
+  ];
 
   public function testDbQueryCount() {
     $queryCountStart = \ProcessWire\Database::getQueryLog();
-    self::execute(self::$performanceQuery);
+    $res = self::execute(self::$performanceQuery['query'], self::$performanceQuery['variables']);
     $queryCountEnd = \ProcessWire\Database::getQueryLog();
     $queryCount = count($queryCountEnd) - count($queryCountStart);
 
@@ -75,8 +82,56 @@ class DbQueryCountTest extends GraphQLTestCase {
     $performanceChangeReport = "\nPerformance Improvement\n$improvementPercent%";
     \ProcessWire\GraphQL\log("$queryCountReport$performanceChangeReport");
 
-    // assert
+    // assert performance
     assertLessThanOrEqual(self::$bestQueryCount, $queryCount);
     assertGreaterThanOrEqual(self::$bestQueryCount / 5, $queryCount, "It can't be true! Performance increased five times!");
+
+    // assert result count
+    $skyscrapers = Utils::pages()->find(self::$performanceQuery['variables']['selector']);
+    assertEquals($skyscrapers->count(), count($res->data->skyscraper->list), 'Incorrect number of skyscrapers fetched.');
+
+    // assert valid skyscraper
+    $expected = $skyscrapers->get("images.count>1, sort=random");
+    assertNotNull($expected, 'No expected skyscraper to check.');
+    $actual = self::getListItemId($res->data->skyscraper->list, $expected->id);
+    assertNotNull($actual, 'No actual skyscraper to check.');
+    assertEquals($actual->id, $expected->id, 'Incorrect id.');
+    assertEquals($actual->title, $expected->title, 'Incorrect title.');
+    assertEquals($actual->height, $expected->height, 'Incorrect height.');
+    assertEquals($actual->floors, $expected->floors, 'Incorrect floors.');
+    assertEquals($actual->year, $expected->year, 'Incorrect year.');
+
+    // assert valid skyscraper images data
+    assertEquals(count($expected->images), count($actual->images), 'Incorrect images amount.');
+    assertEquals($expected->images->first()->url, $actual->images[0]->url, 'Incorrect image url.');
+    assertEquals($expected->images->first()->width, $actual->images[0]->width, 'Incorrect image width.');
+    assertEquals($expected->images->first()->height, $actual->images[0]->height, 'Incorrect image height.');
+    assertEquals($expected->images->first()->description, $actual->images[0]->description, 'Incorrect image description.');
+
+    // assert valid skyscraper architects data
+    $expected = $skyscrapers->find("architects.count>0");
+    $arhitectsIds = $expected->implode('|', 'architects');
+    $architectsWithResumes = Utils::pages()->find("id=$arhitectsIds, resume.count>0");
+    $expected = $skyscrapers->get("architects=$architectsWithResumes, sort=random");
+    assertNotNull($expected, 'No expected skyscraper to check.');
+    $actual = self::getListItemId($res->data->skyscraper->list, $expected->id);
+    assertEquals(count($expected->architects), count($actual->architects), 'Incorrect architects amount.');
+    assertEquals($expected->architects[0]->id, $actual->architects->list[0]->id, 'Incorrect architect id.');
+    assertEquals($expected->architects[0]->title, $actual->architects->list[0]->title, 'Incorrect architect title.');
+    assertEquals($expected->architects[0]->created, $actual->architects->list[0]->created, 'Incorrect architect created.');
+    assertEquals($expected->architects[0]->born, $actual->architects->list[0]->born, 'Incorrect architect born.');
+    assertEquals(count($expected->architects[0]->resume), count($actual->architects->list[0]->resume), 'Incorrect architects resume amount.');
+    assertEquals($expected->architects[0]->resume->first()->url, $actual->architects->list[0]->resume[0]->url, 'Incorrect architect resume url.');
+    assertEquals($expected->architects[0]->resume->first()->description, $actual->architects->list[0]->resume[0]->description, 'Incorrect architect resume description.');
+  }
+
+  public static function getListItemId($list, $id)
+  {
+    foreach ($list as $item) {
+      if ((int) $item->id === (int) $id) {
+        return $item;
+      }
+    }
+    return null;
   }
 }
