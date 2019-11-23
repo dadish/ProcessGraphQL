b9c3d45a6acb4391437a098c61d9ee6c562a5055
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-11-23 17:40:00 +0500

    Increase maxLimit for development server to 500.

--- test/server.php
+++ test/server.php
@@ -45,7 +45,7 @@ function cors() {
 $module = $modules->get('ProcessGraphQL');
 $module->legalTemplates = ['skyscrapers', 'skyscraper', 'architects', 'architect', 'cities', 'city', 'list-all'];
 $module->legalFields = ['abbreviation', 'architects', 'body', 'born', 'email', 'featured', 'floors', 'freebase_guid', 'height', 'images', 'map', 'options', 'options_single', 'resume', 'selected', 'skyscrapers', 'sponsor', 'title', 'wikipedia_id', 'year', 'slides'];
-
+$module->maxLimit = 500;
 
 cors();
 $res = $module->executeGraphQL();
