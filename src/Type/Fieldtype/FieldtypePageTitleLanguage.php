b7b8570c35dab69240cfdc7a5a8387e99e351f83
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-12-07 17:31:28 +0500

    Add FieldtypePageTitleLanguage.

---
+++ src/Type/Fieldtype/FieldtypePageTitleLanguage.php
@@ -0,0 +1,8 @@
+<?php namespace ProcessWire\GraphQL\Type\Fieldtype;
+
+use ProcessWire\GraphQL\Type\Fieldtype\FieldtypePageTitle;
+
+class FieldtypePageTitleLanguage extends FieldtypePageTitle
+{ 
+
+}
