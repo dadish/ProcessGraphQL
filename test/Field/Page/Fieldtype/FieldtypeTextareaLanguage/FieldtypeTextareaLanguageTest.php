43d7919064f4b7083ca561c53dd5c35d6f6575e4
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2019-12-08 13:29:36 +0500

    Add tests for basic language support.

---
+++ test/Field/Page/Fieldtype/FieldtypeTextareaLanguage/FieldtypeTextareaLanguageTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ProcessWire\GraphQL\Test\Field\Page\Fieldtype;
+
+use \ProcessWire\GraphQL\Utils;
+use \ProcessWire\GraphQL\Test\GraphQLTestCase;
+use \ProcessWire\GraphQL\Test\Field\Page\Fieldtype\Traits\FieldtypeTestTrait;
+
+class FieldtypeTextareaLanguageTest extends GraphQLTestCase {
+
+  const settings = [
+    'login' => 'admin',
+    'legalTemplates' => ['basic-page'],
+    'legalFields' => ['address'],
+  ];
+  const FIELD_NAME = 'address';
+  const FIELD_TYPE = 'FieldtypeTextareaLanguage';
+
+  use FieldtypeTestTrait;
+	
+  public function testValue()
+  {
+  	$page = Utils::pages()->get("template=basic-page, address!=''");
+  	$query = "{
+  		basicPage (s: \"id=$page->id\") {
+  			list {
+  				address
+  			}
+  		}
+  	}";
+  	$res = self::execute($query);
+    assertEquals($page->address, $res->data->basicPage->list[0]->address, 'Retrieves address value.');
+    assertObjectNotHasAttribute('errors', $res, 'There are errors.');
+  }
+
+  public function testLanguageValue()
+  {
+  	$page = Utils::pages()->get("template=basic-page, address!=''");
+  	$query = 'query getBasicPage($s: Selector!){
+      language (name: "ru")
+  		basicPage (s: $s) {
+  			list {
+  				address
+  			}
+  		}
+    }';
+    $variable = [
+      's' => "id=$page->id"
+    ];
+  	$res = self::execute($query, $variable);
+    assertEquals($page->getLanguageValue('ru', 'address'), $res->data->basicPage->list[0]->address, 'Retrieves address language value.');
+    assertObjectNotHasAttribute('errors', $res, 'There are errors.');
+  }
+
+}
