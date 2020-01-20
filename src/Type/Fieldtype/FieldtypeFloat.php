7f6356237f88d8c1c04e255d9a6255682e9d5575
SteVO36 <to.us@me.com>
2020-01-20 16:41:10 +0100

    fix error on fields of type FieldtypeFloat that are NULL

--- src/Type/Fieldtype/FieldtypeFloat.php
+++ src/Type/Fieldtype/FieldtypeFloat.php
@@ -1,6 +1,9 @@
 <?php namespace ProcessWire\GraphQL\Type\Fieldtype;
 
 use GraphQL\Type\Definition\Type;
+use ProcessWire\Field;
+use ProcessWire\Page;
+use ProcessWire\GraphQL\Cache;
 use ProcessWire\GraphQL\Type\Fieldtype\Traits\FieldTrait;
 use ProcessWire\GraphQL\Type\Fieldtype\Traits\InputFieldTrait;
 use ProcessWire\GraphQL\Type\Fieldtype\Traits\SetValueTrait;
@@ -13,5 +16,20 @@ class FieldtypeFloat
   public static function type()
   {
     return Type::float();
+  }
+
+  public static function field($field)
+  {
+    return Cache::field($field->name, function () use ($field) {
+      return [
+        'name' => $field->name,
+        'type' => self::type($field),
+        'resolve' => function (Page $page) use ($field) {
+          $fieldName = $field->name;
+          $value = floatval($page->$fieldName);
+          return $value;
+        }
+      ];
+    });
   }
 }
