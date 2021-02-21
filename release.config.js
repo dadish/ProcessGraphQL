d1da62e96cb96204ba090ecd11b67fc2cd1c055a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2021-02-21 07:53:53 +0400

    build(infra): fix semantic-release plugins confuguration

--- release.config.js
+++ release.config.js
@@ -3,11 +3,11 @@ module.exports = {
   plugins: [
     [
       "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
       {
         releaseRules: [{ type: "build", release: "patch" }],
       },
     ],
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/exec",
       {
