c00398d82eab6cc44e8c3309769417a399f66f2c
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2021-02-27 08:12:27 +0400

    build(infra): use stderr for logging semantic-release/exec commands

--- scripts/command.js
+++ scripts/command.js
@@ -1,11 +1,5 @@
 const command = process.argv[2];
-if (!command) {
-  console.error(
-    "Error: Should provide a version argument. See `npm help version`."
-  );
-  process.exit(1);
-}
 
-console.log("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅");
-console.log(command);
-console.log("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅");
+console.error("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅");
+console.error(command);
+console.error("✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅");
